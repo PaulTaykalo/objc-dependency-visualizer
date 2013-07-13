adf813dffdc17c69a4226c68edf2096bc6f931da
Pavel Taykalo <ptaykalo@stanfy.com.ua>
2013-07-14 02:36:26 +0300

    Second commit

--- generate-objc-dependencies-to-json.rb
+++ generate-objc-dependencies-to-json.rb
@@ -1,6 +1,22 @@
 #!/usr/bin/ruby
-SEARCH_DIRECTORY = "."
-EXCLUSION_PREFIXES = "NS|UI"
+if ARGV.first == nil 
+	puts "generate-objc-dependencies-to-json.rb <directory> [exclude_prefixes]"
+	puts " directory - is path to directory where your .o files were put by compiler"
+	puts "   Example :"
+	puts "     ~/Library/Developer/Xcode/DerivedData/MYPROJECT-esemwbhsllglridnecifwkfjbnlh/Build/Intermediates/MYPROJECT.build/Debug-iphoneos/MYPROJECT.build/Objects-normal/armv7"
+	puts " "
+	puts " exclude_prefixes - is a part of regex that will exclude some system frameworks from final visualization"
+	puts "   Default value : NS|UI|CA "
+	puts "   Example :  NS|UI"
+	puts "              UI|CA|MF"
+	exit 1
+end
+
+
+SEARCH_DIRECTORY = ARGV.first
+
+
+EXCLUSION_PREFIXES = ARGV[1] != nil ? ARGV[1] : "NS|UI|CA"
 
 #Header
 puts <<-THEEND
@@ -11,7 +27,7 @@ THEEND
 
 
 #Searching all the .o files and showing its information through nm call
-IO.popen("find #{SEARCH_DIRECTORY} -name \"*.o\" -exec nm -o {} \\;") { |f| 
+IO.popen("find \"#{SEARCH_DIRECTORY}\" -name \"*.o\" -exec nm -o {} \\;") { |f| 
 	 f.each do |line|  
 
 
@@ -27,8 +43,10 @@ IO.popen("find #{SEARCH_DIRECTORY} -name \"*.o\" -exec nm -o {} \\;") { |f|
       			#Capturing filename (We'll think that this is source)
       			#And dependency (We'll think that this is the destination)
 
-      			source,dest = /[^\w]*([^\.]+)\.o.*_OBJC_CLASS_\$_(.*)/.match(line)[1,2]
-                puts "            { \"source\" : \"#{source}\", \"dest\" : \"#{dest}\" },"
+      			source,dest = /[^\w]*([^\.\/]+)\.o.*_OBJC_CLASS_\$_(.*)/.match(line)[1,2]
+      			if source != dest 
+     				puts "            { \"source\" : \"#{source}\", \"dest\" : \"#{dest}\" },"
+      			end
       		end
 	  end
     end

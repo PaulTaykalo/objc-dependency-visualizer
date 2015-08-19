9f658ab5c997442957b88b76bf4031b4ab964932
Paul Taykalo <tt.kilew@gmail.com>
2015-08-19 08:33:02 +0300

    bit updated code, resturctured, Ruby style applied

--- generate-objc-dependencies-to-json.rb
+++ generate-objc-dependencies-to-json.rb
@@ -39,13 +39,12 @@ parser = OptionParser.new do |o|
     options[:swift_dependencies] = v
   }
 
-
   o.separator "Common options:"
   o.on_tail('-h', "Prints this help") { puts o; exit }
   o.parse!
 end
 
-if !options[:search_directory]
+unless options[:search_directory]
   paths = []
 
   # looking for derived data
@@ -75,9 +74,9 @@ if !options[:search_directory]
     end
   end
 
-  paths_sorted_by_time = filtered_by_target_paths.sort_by { |f|
+  paths_sorted_by_time = filtered_by_target_paths.sort_by do |f|
     File.ctime(f.chomp)
-  }
+  end
 
   last_modified_dir = paths_sorted_by_time.last.chomp
   $stderr.puts "Last modifications were in\n#{last_modified_dir}\ndirectory at\n#{File.ctime(last_modified_dir)}"
@@ -85,18 +84,11 @@ if !options[:search_directory]
   options[:search_directory] = last_modified_dir
 end
 
-if !options[:search_directory]
+unless options[:search_directory]
   puts parser.help
   exit 1
 end
 
-
-#Header
-puts <<-THEEND
-   var dependencies = {
-   	links:
-   	  [
-THEEND
 
 links = {}
 
@@ -105,7 +97,7 @@ def is_primitive_swift_type?(dest)
 end
 
 def is_filtered_swift_type?(dest)
-  /(ClusterType|ScalarType|LiteralType)$/.match(dest) != nil #or /^([a-z])/.match(dest) != nil
+  /(ClusterType|ScalarType|LiteralType)$/.match(dest) != nil #or /^([a-z])/.match(dependency) != nil
 end
 
 
@@ -113,69 +105,65 @@ def can_be_used_as_destination(dest, exclusion_prefixs)
   /^(#{exclusion_prefixs})/.match(dest) == nil and /^\w/.match(dest) != nil and !is_primitive_swift_type?(dest) and !is_filtered_swift_type?(dest)
 end
 
+def add_dependency_link(links, source, dest)
+  if source != dest and dest != 'BOOL'
+    destinations = links[source] ? links[source] : (links[source] = {})
+    destinations[dest] = 'set up'
+  end
+end
+
 if options[:swift_dependencies]
   # This thing need to be commented :) It's removes too many connections
   # YAML.add_domain_type("", "private") { |type, val|
   #   'AnyObject'
   # }
-  Dir.glob("#{options[:search_directory]}/*.swiftdeps") do |my_text_file|
-    # puts my_text_file
-    swiftdeps = YAML.load_file(my_text_file)
-    if swiftdeps["provides"] && swiftdeps["provides"].length == 1
-      swiftdeps["provides"].each { |source|
-        destinations = links[source] ? links[source] : (links[source] = {})
-        swiftdeps["top-level"].each { |unparseddest|
-          # puts unparseddest
-          dest = unparseddest
-          if can_be_used_as_destination(dest, options[:exclusion_prefixes])
-            destinations[dest] = "set up"
+  Dir.glob("#{options[:search_directory]}/*.swiftdeps") do |swift_deps_file|
+    # puts swift_deps_file
+    swiftdeps = YAML.load_file(swift_deps_file)
+
+    declared_classes = swiftdeps[:provides]
+    dependencies = swiftdeps[:'top-level']
+
+    if declared_classes
+      if declared_classes.length == 1
+        source = declared_classes.first
+        dependencies.each do |dependency|
+          if can_be_used_as_destination(dependency, options[:exclusion_prefixes])
+            add_dependency_link(links, source, dependency)
           end
-        }
-      }
-    elsif swiftdeps["provides"]
-
-      classes_declared_in_file = swiftdeps["provides"]
-
-      filename = '< ' + File.basename(my_text_file, ".swiftdeps") +' >'
-      swiftdeps["provides"].each { |source|
-        destinations = links[source] ? links[source] : (links[source] = {})
-        destinations[filename] = "set up"
-      }
-
-      source = filename
-      destinations = links[source] ? links[source] : (links[source] = {})
-      swiftdeps["top-level"].each { |unparseddest|
-        # puts unparseddest
-        dest = unparseddest
-        if can_be_used_as_destination(dest, options[:exclusion_prefixes]) and not classes_declared_in_file.include?(dest)
-          destinations[dest] = "set up"
         end
-      }
+      else
 
+        # Linking all classes as dependent items of this file
+        filename = '< ' + File.basename(swift_deps_file, '.swiftdeps') +' >'
+        declared_classes.each do |clz|
+          add_dependency_link(links, clz, filename)
+        end
+
+        # Linking all dependencies to to
+        dependencies.each do |dependency|
+          if can_be_used_as_destination(dependency, options[:exclusion_prefixes]) and not declared_classes.include?(dependency)
+            add_dependency_link(links, filename, dependency)
+          end
+        end
+      end
     end
   end
 else
   #Searching all the .o files and showing its information through nm call
   IO.popen("find \"#{options[:search_directory]}\" -name \"*.o\" -exec  /usr/bin/nm -o {} \\;") { |f|
-    f.each do |line|
+    f.each do |symbol_name_string|
 
       # Gathering only OBC_CLASSES
-      match = /_OBJC_CLASS_\$_/.match line
-
-      if match != nil
-        exclusion_match = /_OBJC_CLASS_\$_(#{options[:exclusion_prefixes]})/.match line
+      if symbol_name_string =~ /_OBJC_CLASS_\$_/
 
         # Excluding base frameworks prefixes
-        if exclusion_match == nil
+        unless symbol_name_string =~ /_OBJC_CLASS_\$_(#{options[:exclusion_prefixes]})/
 
-          #Capturing filename (We'll think that this is source)
-          #And dependency (We'll think that this is the destination)
-
-          source, dest = /[^\w]*([^\.\/]+)\.o.*_OBJC_CLASS_\$_(.*)/.match(line)[1, 2]
-          if source != dest
-            destinations = links[source] ? links[source] : (links[source] = {})
-            destinations[dest] = "set up"
-          end
+          #Capturing filename (We'll treat that as a source)
+          #And dependency (We'll treat it as a destination)
+          source, dest = /[^\w]*([^\.\/]+)\.o.*_OBJC_CLASS_\$_(.*)/.match(symbol_name_string)[1, 2]
+          add_dependency_link(links, source, dest)
         end
       end
     end
@@ -184,39 +172,43 @@ else
   if options[:use_dwarf]
 
     # Search files again
-    IO.popen("find \"#{options[:search_directory]}\" -name \"*.o\"") { |f|
-      f.each do |line|
+    IO.popen("find \"#{options[:search_directory]}\" -name \"*.o\"") do |f|
+      f.each do |object_file_path|
 
-        # puts "Running dwarfdump #{line} | grep -A1 TAG_pointer_type"
-        source = /.*\/(.+)\.o/.match(line)[1]
-        IO.popen("dwarfdump #{line.strip} | grep -A1 TAG_pointer_type") { |fd|
-          fd.each do |line2|
+        # puts "Running dwarfdump #{object_file_path} | grep -A1 TAG_pointer_type"
+        source = /.*\/(.+)\.o/.match(object_file_path)[1]
+
+        IO.popen("dwarfdump #{object_file_path.strip} | grep -A1 TAG_pointer_type") do |fd|
+          fd.each do |type_definition|
             # Finding the name in types
             # AT_type( {0x00000456} ( objc_object ) )
-            name = /.*?AT_type\(\s\{.*?\}.*\(\s((function|const)\s)?([A-Z][^\)]+?)\*?\s\).*/.match(line2)
+            name = /.*?AT_type\(\s\{.*?\}.*\(\s((function|const)\s)?([A-Z][^\)]+?)\*?\s\).*/.match(type_definition)
             if name != nil
               dest = name[3]
               if can_be_used_as_destination(dest, options[:exclusion_prefixes])
-                if source != dest and dest != "BOOL"
-                  destinations = links[source] ? links[source] : (links[source] = {})
-                  destinations[dest] = "set up"
-                end
+                add_dependency_link(links, source, dest)
               end
             end
           end
-        }
+        end
       end
-    }
+    end
   end
 end
 
+#Header
+puts <<-THEEND
+   var dependencies = {
+   	links:
+   	  [
+THEEND
 
 sources_count = links.length
 links_count = 0
 links.each do |source, dest_hash|
   links_count = links_count + dest_hash.length
   dest_hash.each do |dest, _|
-    puts "            { \"source\" : \"#{source}\", \"dest\" : \"#{dest}\" },"
+    puts "            { \"source\" : \"#{source}\", \"dependency\" : \"#{dest}\" },"
   end
 end
 

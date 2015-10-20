29f9a60d70b1803c0649f01c8732bbe3564523d3
Paul Taykalo <tt.kilew@gmail.com>
2015-10-20 10:48:13 +0300

    restructurise graph actions, make it more convenient and structured

--- Scripts/graph-actions-select.js
+++ Scripts/graph-actions-select.js
@@ -1,201 +1,195 @@
-
 //  ===================================================
 //  ===============  SELECTING_NODE       =============
 //  ===================================================
 
-var selectedIdx = -1
-var selectedType = "normal"
-var selectedobject = {}
+!function () {
+    var graph_actions = {
 
-function deselect_node(svg, d) { 
-  delete d.fixed
-  selectedIdx = -1
-  selectedobject = {};
-  svg.selectAll('circle, path, text')
-    .classed('filtered', false)
-    .each(function(node) {
-     node.filtered = false
-    })
-    .transition()
+        create: function (svg) {
+            return {
+                selectedIdx: -1,
+                selectedType: "normal",
+                svg: svg,
+                selectedObject: {},
 
-  svg.selectAll('.link')
-    .attr("marker-end", "url(#default)")      
-    .classed('filtered', false)
-    .transition()
+                deselect_node: function (d) {
+                    delete d.fixed;
+                    this.selectedIdx = -1;
+                    this.selectedObject = {};
+                    this.svg.selectAll('circle, path, text')
+                        .classed('filtered', false)
+                        .each(function (node) {
+                            node.filtered = false
+                        })
+                        .transition();
 
+                    this.svg.selectAll('.link')
+                        .attr("marker-end", "url(#default)")
+                        .classed('filtered', false)
+                        .transition();
+                },
 
-  force.start();
-  return
-}
+                deselect_selected_node: function () {
+                    this.deselect_selected_node(this.selectedObject)
+                },
 
-function deselect_selected_node(svg) {
-	deselect_node(svg, selectedobject)
-}
+                _selectAndLockNode: function (node, type) {
+                    delete this.selectedObject.fixed;
+                    this.selectedIdx = node.idx;
+                    this.selectedObject = node;
+                    this.selectedObject.fixed = true;
+                    this.selectedType = type;
+                },
 
-function select_node(svg, d) { 
-  if (d3.event.defaultPrevented) return
+                _deselectNodeIfNeeded: function (node, type) {
+                    if (node.idx == this.selectedIdx && this.selectedType == type) {
+                        this.deselect_node(node);
+                        return true;
+                    }
+                    return false;
+                },
 
-  // Deselect if needed
-  if (d.idx == selectedIdx && selectedType == "normal") { deselect_node(svg, d); return }
+                _fadeOutAllNodesAndLinks: function () {
+                    // Fade out all circles
+                    this.svg.selectAll('circle')
+                        .classed('filtered', true)
+                        .each(function (node) {
+                            node.filtered = true;
+                            node.neighbours = false;
+                        }).transition();
 
-  // Update selected object
-  delete selectedobject.fixed
-  selectedIdx = d.idx
-  selectedobject = d
-  selectedobject.fixed = true
-  selectedType = "normal"
+                    this.svg.selectAll('text')
+                        .classed('filtered', true)
+                        .transition();
 
-  // Figure out the neighboring node id's with brute strength because the graph is small
-  var nodeNeighbors = 
-  dependecy_graph.links
-    .filter(function(link) {
-        return link.source.index === d.index || link.target.index === d.index;})
-    .map(function(link) {
-        return link.source.index === d.index ? link.target.index : link.source.index; 
-      }
-    );
+                    this.svg.selectAll('.link').
+                        transition()
+                        .attr("marker-end", "");
 
-  // Fade out all circles
-  svg.selectAll('circle')
-  .classed('filtered', true)
-  .each(function(node){
-    node.filtered = true;
-    node.neighbours = false;
-  }).transition()
+                },
 
-  svg.selectAll('text')
-    .classed('filtered', true)
-    .transition()
+                _highlightNodesWithIndexes: function (indexesArray) {
+                    this.svg.selectAll('circle, text')
+                        .filter((node) => indexesArray.indexOf(node.index) > -1)
+                        .classed('filtered', false)
+                        .each((node) => {
+                            node.filtered = false;
+                            node.neighbours = true;
+                        })
+                        .transition();
+                },
 
+                _highlightLinksFromNode: function (node) {
+                    this.svg.selectAll('.link')
+                        .filter((link) => link.source.index === node.index || link.target.index == node.index)
+                        .classed('filtered', false)
+                        .attr("marker-end", (l) => l.source.index === node.index ? "url(#dependency)" : "url(#dependants)")
+                        .transition();
+                },
 
-  svg.selectAll('.link').
-    transition()
-    .attr("marker-end", "")
+                _highlightLinksFromRootWithNodes: function (root, nodeNeighbors) {
+                    this.svg.selectAll('.link')
+                        .filter((link) => nodeNeighbors.indexOf(link.source.index) > -1)
+                        .classed('filtered', false)
+                        .attr("marker-end", (l) => l.source.index === root.index ? "url(#dependency)" : "url(#dependants)")
+                        .transition();
+                },
 
+                select_node: function (d) {
+                    if (d3.event.defaultPrevented) {
+                        return
+                    }
+                    if (this._deselectNodeIfNeeded(d, "normal")) {
+                        return
+                    }
+                    this._selectAndLockNode(d, "normal");
+                    // Update selected object
 
-  // Higlight all circle and texts
-  svg.selectAll('circle, text')
-    .filter(function(node) {
-        return nodeNeighbors.indexOf(node.index) > -1 || node.index == d.index;
-    })
-    .classed('filtered', false)
-    .each(function(node) {
-       node.filtered = false;
-       node.neighbours = true;
-    })
-    .transition()
+                    // Figure out the neighboring node id's with brute strength because the graph is small
+                    var nodeNeighbors =
+                        dependecy_graph.links
+                            .filter((link) => link.source.index === d.index || link.target.index === d.index)
+                            .map((link) => link.source.index === d.index ? link.target.index : link.source.index);
 
-  // Higlight links
-  svg.selectAll('.link')
-    .filter(function(link) {
-      return link.source.index === d.index || link.target.index == d.index
-    })
-    .classed('filtered', false)
-    .attr("marker-end", function(l) { return l.source.index === d.index ? "url(#dependency)" : "url(#dependants)"})
-    .transition()
+                    nodeNeighbors.push(d.index);
 
-  force.start();
-}
+                    this._fadeOutAllNodesAndLinks();
+                    this._highlightNodesWithIndexes(nodeNeighbors);
+                    this._highlightLinksFromNode(d);
+                },
 
-function select_recursively_node(svg, d) { 
-  if (d3.event.defaultPrevented) return
-  
-  // Don't show context menu :)
-  d3.event.preventDefault()  
+                select_recursively_node: function (node) {
+                    if (d3.event.defaultPrevented) {
+                        return
+                    }
 
-  // Deselect if needed
-  if (d.idx == selectedIdx && selectedType == "recursive") { deselect_node(svg, d); return }
+                    // Don't show context menu :)
+                    d3.event.preventDefault();
 
-  // Update selected object
-  delete selectedobject.fixed
-  selectedIdx = d.idx
-  selectedobject = d
-  selectedobject.fixed = true
-  selectedType = "recursive"
+                    if (this._deselectNodeIfNeeded(node, "recursive")) {
+                        return
+                    }
 
-  // Figure out the neighboring node id's with brute strength because the graph is small
-  var neighbours = {}
-  var nodeNeighbors = 
-  dependecy_graph.links
-    .filter(function(link) {
-        return link.source.index === d.index})
-    .map(function(link) {
-        var idx = link.source.index === d.index ? link.target.index : link.source.index;
-        if (link.source.index === d.index) {
-          console.log("Step 0. Adding ",dependecy_graph.nodes[idx].name)
-          neighbours[idx] = 1;
+                    this._selectAndLockNode(node, "recursive");
+
+                    // Figure out the neighboring node id's with brute strength because the graph is small
+                    var neighbours = {};
+                    var nodeNeighbors =
+                        dependecy_graph.links
+                            .filter((link) => link.source.index === node.index)
+                            .map((link) => {
+                                var idx = link.source.index === node.index ? link.target.index : link.source.index;
+                                if (link.source.index === node.index) {
+                                    console.log("Step 0. Adding ", dependecy_graph.nodes[idx].name);
+                                    neighbours[idx] = 1;
+                                }
+                                return idx;
+                            }
+                        );
+
+                    // Next part - neighbours of neigbours
+                    var currentsize = Object.keys(neighbours).length;
+                    var nextSize = 0;
+                    var step = 1;
+                    while (nextSize != currentsize) {
+                        console.log("Current size " + currentsize + " Next size is " + nextSize);
+                        currentsize = nextSize;
+                        dependecy_graph.links
+                            .filter(function (link) {
+                                return neighbours[link.source.index] != undefined
+                            })
+                            .map(function (link) {
+                                var idx = link.target.index;
+                                console.log("Step " + step + ". Adding ", dependecy_graph.nodes[idx].name + " From " + dependecy_graph.nodes[link.source.index].name);
+
+                                neighbours[idx] = 1;
+                                return idx;
+                            }
+                        );
+                        nextSize = Object.keys(neighbours).length;
+                        step = step + 1
+                    }
+
+                    neighbours[node.index] = 1;
+                    nodeNeighbors = Object.keys(neighbours).map(function (neibour) {
+                        return parseInt(neibour);
+                    });
+                    nodeNeighbors.push(node.index);
+
+                    this._fadeOutAllNodesAndLinks();
+                    this._highlightNodesWithIndexes(nodeNeighbors);
+                    this._highlightLinksFromRootWithNodes(node, nodeNeighbors);
+                }
+
+            };
         }
-        return idx; 
-      }
-    );
+    };
 
-  // Next part - neighbours of neigbours
-  var currentsize = Object.keys(neighbours).length
-  var nextSize = 0;
-  var step = 1;
-  while (nextSize != currentsize) {
-    console.log("Current size " + currentsize + " Next size is " + nextSize)
-    currentsize = nextSize
-    dependecy_graph.links
-        .filter(function(link) {
-            return neighbours[link.source.index] != undefined})
-        .map(function(link) {
-            var idx = link.target.index;
-            console.log("Step "+step+". Adding ",dependecy_graph.nodes[idx].name + " From " + dependecy_graph.nodes[link.source.index].name )
-
-            neighbours[idx] = 1;
-            return idx;
-          }
-        );
-     nextSize = Object.keys(neighbours).length   
-     step = step + 1
-  }
-
-  neighbours[d.index] = 1
-  nodeNeighbors = Object.keys(neighbours).map(function(neibour) {
-      return parseInt(neibour);
-  })
-
-
-  // Fade out all circles
-  svg.selectAll('circle')
-  .classed('filtered', true)
-  .each(function(node){
-    node.filtered = true;
-    node.neighbours = false;
-  }).transition()
-    
-
-  svg.selectAll('text')
-    .classed('filtered', true)
-    .transition()
-  
-
-  svg.selectAll('.link').
-    transition()
-    .attr("marker-end", "")
-
-
-  // Higlight all circle and texts
-  svg.selectAll('circle, text')
-    .filter(function(node) {
-        return nodeNeighbors.indexOf(node.index) > -1 || node.index == d.index;
-    })
-    .classed('filtered', false)
-    .each(function(node) {
-       node.filtered = false;
-       node.neighbours = true;
-    })
-    .transition()
-
-  // Higlight links
-  svg.selectAll('.link')
-    .filter(function(link) {
-      return nodeNeighbors.indexOf(link.source.index) > -1
-    })
-    .classed('filtered', false)
-    .attr("marker-end", function(l) { return l.source.index === d.index ? "url(#dependency)" : "url(#dependants)"})
-    .transition()
-
-  force.start();
-}
+    if (typeof define === "function" && define.amd) {
+        define(graph_actions);
+    } else if (typeof module === "object" && module.exports) {
+        module.exports = graph_actions;
+    } else {
+        this.graph_actions = graph_actions;
+    }
+}();
--- Scripts/parse.js
+++ Scripts/parse.js
@@ -18,7 +18,7 @@
 
       addLink:function(link) {
 
-        var source_node = this.getNode(link.source)
+        var source_node = this.getNode(link.source);
         source_node.source++;
 
         var dest_node = this.getNode(link.dest);
@@ -38,7 +38,7 @@
       getNode:function(nodeName) {
         var node = this.nodesSet[nodeName];
         if (node == null) {
-          var idx = this.node_index
+          var idx = this.node_index;
           this.nodesSet[nodeName] = node = { idx :idx, name: nodeName, source: 1, dest: 0  };
           this.node_index++;
         }
@@ -56,8 +56,8 @@
       }
     };
 
-  }
-  
+  };
+
   objcdv._createPrefixes = function() {
     return {
       _prefixesDistr:{},
@@ -65,7 +65,7 @@
       _sortedPrefixes:null,
 
       addName:function(name) {
-        this._sortedPrefixes = null
+        this._sortedPrefixes = null;
 
         var prefix = name.substring(0, 2);
         if (!(prefix in this._prefixesDistr)) {
@@ -76,7 +76,7 @@
       },
 
       prefixIndexForName:function(name) {
-        var sortedPrefixes = this._getSortedPrefixes()
+        var sortedPrefixes = this._getSortedPrefixes();
         var prefix = name.substring(0, 2);
         return _.indexOf(sortedPrefixes, prefix)
       },
@@ -90,29 +90,29 @@
         return this._sortedPrefixes
       }
     };
-  }
+  };
 
   objcdv.parse_dependencies_graph = function (dependencies) {
 
-    var graph = this._createGraph()
-    var prefixes = this._createPrefixes()
+    var graph = this._createGraph();
+    var prefixes = this._createPrefixes();
 
     dependencies.links.forEach((link) => {
-      graph.addLink(link)
+      graph.addLink(link);
 
       prefixes.addName(link.source);
       prefixes.addName(link.dest);
 
-    })
+    });
 
     graph.updateNodes((node) => {
       node.weight = node.source;
       node.group = prefixes.prefixIndexForName(node.name) + 1
-    })
+    });
 
     return graph.d3jsGraph()
 
-  }
+  };
 
   if (typeof define === "function" && define.amd) {
     define(objcdv);

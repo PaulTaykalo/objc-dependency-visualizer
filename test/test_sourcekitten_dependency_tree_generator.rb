a10273286e63d1fd3dbe4734d1d962c1c6bec5d3
Paul Taykalo <tt.kilew@gmail.com>
2017-03-06 08:41:24 +0200

    Multiple files

--- test/test_sourcekitten_dependency_tree_generator.rb
+++ test/test_sourcekitten_dependency_tree_generator.rb
@@ -7,7 +7,7 @@ class SwiftDependencyTreeGeneratorTest < Test::Unit::TestCase
     assert_equal generator.find_dependencies, {}
   end
 
-  def test_swift_simple_objects
+  def test_simple_objects
     generator = ObjCDependencyTreeGenerator.new({
                                                     :sourcekitten_dependencies_file => './test/fixtures/sourcekitten/sourcekitten.json',
                                                 })
@@ -17,12 +17,11 @@ class SwiftDependencyTreeGeneratorTest < Test::Unit::TestCase
     assert_not_nil(dependencies['MainClass'])
     assert_not_nil(dependencies['SubclassOfSubclass'])
     assert_not_nil(dependencies['Subclass'])
-    assert_not_nil(dependencies['ViewController'])
 
   end
 
 
-  def test_swift_simple_inheritance
+  def test_simple_inheritance
     generator = ObjCDependencyTreeGenerator.new({
                                                     :sourcekitten_dependencies_file => './test/fixtures/sourcekitten/sourcekitten.json',
                                                 })
@@ -38,7 +37,7 @@ class SwiftDependencyTreeGeneratorTest < Test::Unit::TestCase
 
   end
 
-  def test_swift_extensions
+  def test_extensions
     generator = ObjCDependencyTreeGenerator.new({
                                                     :sourcekitten_dependencies_file => './test/fixtures/sourcekitten/sourcekitten.json',
                                                 })
@@ -49,7 +48,7 @@ class SwiftDependencyTreeGeneratorTest < Test::Unit::TestCase
 
   end
 
-  def test_swift_structs
+  def test_structs
     generator = ObjCDependencyTreeGenerator.new({
                                                     :sourcekitten_dependencies_file => './test/fixtures/sourcekitten/sourcekitten.json',
                                                 })
@@ -58,6 +57,19 @@ class SwiftDependencyTreeGeneratorTest < Test::Unit::TestCase
     assert_not_nil(dependencies['SimpleStruct'])
     assert_not_nil(dependencies['StructWithProtocols']["ProtocolToExtend"])
     assert_not_nil(dependencies['StructWithProtocols']["AwesomeProtocol"])
+  end
+
+  def test_interfile_dependencies
+    generator = ObjCDependencyTreeGenerator.new({
+                                                    :sourcekitten_dependencies_file => './test/fixtures/sourcekitten/sourcekitten.json',
+                                                })
+    dependencies = generator.find_dependencies
+    assert_not_equal(dependencies, {})
+    assert_not_nil(dependencies['SecondClass'])
+    assert_not_nil(dependencies['SecondClassProtocol'])
+    assert_not_nil(dependencies['SecondClass']["MainClass"])
+    assert_not_nil(dependencies['SecondClass']["SecondClassProtocol"])
+    assert_not_nil(dependencies['SecondClass']["AwesomeProtocol"])
   end
 
 
